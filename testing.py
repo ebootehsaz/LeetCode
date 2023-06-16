d1423ccb3a93d41a2d9be84a1cf7a7fc51521731
Ethan Bootehsaz <ethandb2024@gmail.com>
2023-06-15 21:21:43 -0700

    testing function

---
+++ testing.py
@@ -0,0 +1,20 @@
+def tests(function, extraInfo=False):
+    
+    tests = []
+
+    results = []
+
+    for index, test in enumerate(tests):
+        attempt = function(test)
+        if extraInfo:
+            print("\nTest: {} \nAttempt: {} \nPassed: {}".format(test, attempt, results[index] == attempt))
+        else:
+            print("Passed {}: {}".format(index, results[index] == attempt))
+    print()
+
+
+
+
+
+if __name__ == "__main__":
+    tests()
